be7115c6bb02cf5b769e3eb9aa6eb6d9c19678ca
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2024-01-08 05:17:25 +0000

    API call to display location works

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from "express";
-import axios from "axios";
-import {functions} from "date-fns";
+import {format} from "date-fns";
 
 const app = express();
 
@@ -12,6 +11,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 /* ALL YOUR SERVER STUFF WILL GO HERE */
+
+const apiKey = "5a27d69e27f140f58e7141821230211";
+const location = "sproughton"
+const days = "1"
+
+app.post("/search", async(req, res) => {
+    const searchTerm = req.body.search.toLowerCase();
+
+    try{
+        const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${searchTerm}&days=${days}`
+        const response = await fetch(apiUrl);
+        
+        if(!response.ok){
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        console.log(searchTerm);
+        const weatherDetails = await response.json();
+        console.log("Weather Details:", weatherDetails);
+        const location = `${weatherDetails.location.name}, ${weatherDetails.location.country}`
+        res.status(200).send(location)
+    } catch (error) {
+        console.error("There was an error fetching the data", error);
+        res.status(500).send("Error fetching data");
+    }
+});
 
 // Start the server
 app.listen(3000, () => {
